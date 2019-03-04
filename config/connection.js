1f05f5a2e162e7284db2e602c5b536504d82d331
scc85 <sarahchristinec85@gmail.com>
2019-03-04 13:46:52 -0700

    fix connection

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
-  conncetion = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
 };
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
